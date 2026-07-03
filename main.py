ed46491288c282f8318e520b387fe93e7ae20b6c
Asha <guptaaasha2618@gmail.com>
2026-07-03 13:50:23 +0530

    learning fast API

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI,Path ,HTTPException , Query
+import json
+app = FastAPI()
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+@app.get("/")
+def hello():
+    return {"message": "Patinet mannager System API"}
+@app.get("/about")
+def about():
+    return {"message": "A fully functional API to mangae your patient  records"}
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str = Path(..., description="The ID of the patient to retrieve", example="P001")):
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="Patient not found")
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description="The field to sort by"),order: str = Query("asc", description="The order of sorting (asc or desc)")):
+    valid_fields = ["height", "weight", "bmi"]
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail="Invalid sort field")
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code=400, detail="Invalid sort order")
+    
+    data = load_data()
+    sort_order = True if order == "desc" else False
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by,0), reverse= sort_order)
+    return sorted_data
